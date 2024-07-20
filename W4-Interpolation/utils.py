f615814981ae6b842e3bf7e363d4276cf34b89e0
Andiko-K <andikoputra27@gmail.com>
2024-07-20 13:02:10 +0000

    Lagrange Interpolation

--- W4-Interpolation/utils.py
+++ W4-Interpolation/utils.py
@@ -4,6 +4,14 @@ from IPython.display import HTML
 import unittest
 import timeout_decorator
 
+def generate_points(func, num, left_bound = -1, right_bound = 1):
+    x = np.linspace(left_bound, right_bound, num)
+    y = func(x)
+    points = np.c_[x,y]
+    return points
+
+piecewise_data = generate_points(lambda x: x**2, 10)
+
 class TestLinearInt(unittest.TestCase):
     def __init__(self, methodName='runTest', func=None):
         super(TestLinearInt, self).__init__(methodName)
@@ -67,11 +75,47 @@ def run_tests2(func):
     
     runner = unittest.TextTestRunner()
     runner.run(suite)
-
-def generate_points(func, num, left_bound = -1, right_bound = 1):
-    x = np.linspace(left_bound, right_bound, num)
-    y = func(x)
-    points = np.c_[x,y]
-    return points
-
-piecewise_data = generate_points(lambda x: x**2, 10)
+    
+class TestLagrangeInt(unittest.TestCase):
+    def __init__(self, methodName='runTest', func=None):
+        super(TestLagrangeInt, self).__init__(methodName)
+        self.lagrange_int = func
+    
+    @timeout_decorator.timeout(1)
+    def test_case_1(self):
+        func = lambda x: 18*x+39
+        points = generate_points(func, 2, left_bound = -10, right_bound = 10)
+        x = 7
+        expected_result = func(x)
+        result = self.lagrange_int(x, points)
+        
+        self.assertAlmostEqual(result, expected_result, delta = 1e-3)
+        
+    @timeout_decorator.timeout(1)
+    def test_case_2(self):
+        func = lambda x: x**5+14*x**4+39*x**2+11
+        points = generate_points(func, 6, left_bound = -10, right_bound = 10)
+        x = 4
+        expected_result = func(x)
+        result = self.lagrange_int(x, points)
+        
+        self.assertAlmostEqual(result, expected_result, delta = 1e-3)
+    
+    @timeout_decorator.timeout(1)
+    def test_case_3(self):
+        func = lambda x: np.cos(x)
+        points = generate_points(func, 16, left_bound = -10, right_bound = 10)
+        x = 5
+        expected_result = func(x)
+        result = self.lagrange_int(x, points)
+        
+        self.assertAlmostEqual(result, expected_result, delta = 1e-3)
+    
+def run_tests3(func):
+    suite = unittest.TestSuite()
+    suite.addTest(TestLagrangeInt('test_case_1', func=func))
+    suite.addTest(TestLagrangeInt('test_case_2', func=func))
+    suite.addTest(TestLagrangeInt('test_case_3', func=func))
+    
+    runner = unittest.TextTestRunner()
+    runner.run(suite)
